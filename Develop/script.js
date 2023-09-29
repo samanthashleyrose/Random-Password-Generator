0d12328f3f991b70a04f70aeb656b02bb54f69f3
samanthashleyrose <samantha.rose327@gmail.com>
2023-09-29 16:51:17 -0400

    Declared variables for length min/max and character types.

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,35 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); // gen button HTML
+
+let lengthMin = 8 ;
+let lengthMax = 128 ;
+var charLower = "abcdefghijklmnopqrstuvwxyz";
+var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charNum = "0123456789";
+var charSpecial = "!@#$%^&*()";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); // text box HTML
 
   passwordText.value = password;
+}
+
+//Write code here
+
+
+function generatePassword () {
 
 }
 
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
